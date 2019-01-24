f61987338810541b6f639d031e4c9738e7e3954d
eegeh <300849214@cencol.ca>
2019-01-24 15:43:17 -0500

    updated

---
+++ functions.js
@@ -0,0 +1,14 @@
+var hamburgerPrice = 6.99;
+var chickenburgerPrice = 6.99;
+var veggieburgerPrice = 7.99;
+var smallFry = 1.99;
+var medFry = 2.99;
+var bigFry = 3.99;
+var smallDrink = 0.99;
+var medDrink = 1.99;
+var bigDrink = 2.99;
+
+function thing {
+    
+} 
+
