2edf8358619c0d467de5321f73079dbb5ea44a40
DrDiabolical <egeh2x@gmail.com>
2019-01-30 15:03:43 -0500

    deleted jquery

---
+++ calendar.js
@@ -0,0 +1,18 @@
+var daysofWeek = ["Sunday", "Monday", "Tuesday", "Tuesday",
+    "Wednesday", "Thursday", "Friday", "Saturday"];
+
+var customers = ["Customer ", "Customer ", "Customer ", "Customer ",
+    "Customer ", "Customer ", "Customer ",
+    "Customer ", "Customer ", "Customer ",
+    "Customer ", "Customer ", "Customer ",
+    "Customer ", "Customer ", "Customer ",
+    "Customer ", "Customer ", "Customer ",
+    "Customer ", "Customer ", "Customer ",
+    "Customer ", "Customer ", "Customer ", "Customer ",
+    "Customer ", "Customer ", "Customer ", "Customer ",];
+
+var inOrOut = ["Eat-in", "Eat-in", "Eat-in", "Eat-in", "Take out", "Take out",
+    "Eat-in", "Eat-in", "Take out", "Take out", "Eat-in", "Take out",
+    "Take out", "Eat-in", "Eat-in", "Take out", "Take out", "Take out", "Take out",
+    "Take out", "Eat-in", "Take out", "Eat-in", "Take out", "Eat-in",
+    "Eat-in", "Eat-in", "Eat-in", "Take out", "Eat-in"];
