b594b9cc01ac1e8fb7708fcebe0a38724125f521
DrDiabolical <egeh2x@gmail.com>
2019-02-07 01:58:07 -0500

    finished calendar page

--- calendar.js
+++ calendar.js
@@ -1,8 +1,8 @@
 var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Tuesday",
     "Wednesday", "Thursday", "Friday", "Saturday"];
 
-var customers = ["Customer 1", "Customer 2", "Customer 3", "Customer 4",
-    "Customer 5", "Customer 6", "Customer 7"];
+var customers = ["Jim Bob", "Marissa Jones", "Guy Vazquez", "Toni Miller",
+    "Ron Lee", "A Robot"];
 
 var inOrOut = ["Eat-in", "Eat-in", "Eat-in", "Eat-in", "Take out", "Take out",
     "Eat-in", "Eat-in", "Take out", "Take out", "Eat-in", "Take out",
@@ -10,7 +10,7 @@ var inOrOut = ["Eat-in", "Eat-in", "Eat-in", "Eat-in", "Take out", "Take out",
     "Take out", "Eat-in", "Take out", "Eat-in", "Take out", "Eat-in",
     "Eat-in", "Eat-in", "Eat-in", "Take out", "Eat-in"];
 
-var food = ["Spaghetti", "Hamburger", "Fried chicken", ]
+var food = ["Hamburger", "Chicken burger", "Veggie burger", "Fries", "Coffee", "Tea", "Soda" ]
 
 function addColumnHeaders () {
     var i = 0;
@@ -21,24 +21,18 @@ function addColumnHeaders () {
     }
 }
 
-function randomCustomer() {
+function calendarPopulator() {
     var i = 0;
     while (i < 30) {
-        document.getElementsByTagName("p")[i].innerHTML 
+        document.getElementsByClassName("customer")[i].innerHTML 
         = customers[Math.floor(Math.random()*customers.length)];
-        document.getElementsByTagName("p")[i].innerHTML 
+        document.getElementsByClassName("item")[i].innerHTML 
+        = food[Math.floor(Math.random()*food.length)];
+        document.getElementsByClassName("eatWhere")[i].innerHTML 
         = inOrOut[Math.floor(Math.random()*inOrOut.length)];
         i++;
     }
 }
 
-function randomInOrOut() {
-    return inOrOut[Math.floor(Math.random()*inOrOut.length)];
-}
-
-function randomFood() {
-    return food[Math.floor(Math.random()*food.length)];
-}
-
 window.addEventListener ("load", addColumnHeaders, false);
-window.addEventListener ("load", randomCustomer, false);
+window.addEventListener ("load", calendarPopulator, false);
