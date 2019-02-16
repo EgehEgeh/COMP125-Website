8fb3b2908dff00034737bb48af9b1c4ae2483740
DrDiabolical <egeh2x@gmail.com>
2019-02-15 20:07:06 -0500

    finished phase 3

--- gallery.js
+++ gallery.js
@@ -5,3 +5,13 @@ thumbs.forEach(img => img.addEventListener('click', imgClick));
 function imgClick (e) {
     bigImage.src = e.target.src;
 }
+
+function imageOpen () {
+    window.open(largeImage.src, "Food", "height=1500 width=200");
+}
+
+function imageClose () {
+    expandedImage.Close();
+}
+
+largeImage.addEventListener("click", imageOpen, false);
